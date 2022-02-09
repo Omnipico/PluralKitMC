a596a1a4108ac842ab32c22d8f314b0c2192d39e
HolyAntimony <maya@alyssa.ws>
2022-02-08 21:12:53 -0500

    Add options for logging usernames, and keeping original message event
    Resolves TODO on #2, adding the config option log_username which will prepend logged messages with (username).
    Fixes bug wherein a user sending a message containing %member% will have it replaced with their system hover.
    Adds config option keep_original_message_event, which will keep the original message event but attempt to clear its recipient. In this case, the original message is logged to console.

--- src/main/java/com/omnipico/pluralkitmc/ProxyListener.java
+++ src/main/java/com/omnipico/pluralkitmc/ProxyListener.java
@@ -50,10 +50,63 @@ public class ProxyListener implements Listener {
         }
     }
 
+    private BaseComponent[] getOutputComponent(BaseComponent[] resultComponents, Player player, PluralKitSystem system, String memberName) {
+        return getOutputComponent(resultComponents, player, system, memberName, false);
+    }
+
+    private BaseComponent[] getOutputComponent(BaseComponent[] resultComponents, Player player, PluralKitSystem system, String memberName, Boolean logUsername) {
+        ArrayList<BaseComponent> components = new ArrayList<>();
+        boolean convertedMember = false;
+        if (logUsername) {
+            components.add(new TextComponent("(" + player.getName() + ") "));
+        }
+        for (BaseComponent component : resultComponents) {
+            if (!convertedMember && component.toPlainText().startsWith("%member%")) {
+                convertedMember = true;
+                ComponentBuilder hoverTextBuilder = new ComponentBuilder("User: ").color(ChatColor.GREEN)
+                        .append(TextComponent.fromLegacyText(player.getDisplayName())).append("\nSystem: ").color(ChatColor.GREEN);
+                if (system.getName() != null && system.getName().length() > 0) {
+                    hoverTextBuilder.append(system.getName()).color(ChatColor.AQUA);
+                } else {
+                    hoverTextBuilder.append(system.getId()).color(ChatColor.GRAY);
+                }
+                Text hoverText = new Text(hoverTextBuilder.create());
+                components.addAll(Arrays.asList(new ComponentBuilder(memberName)
+                        .color(component.getColor())
+                        .bold(component.isBold())
+                        .italic(component.isItalic())
+                        .obfuscated(component.isObfuscated())
+                        .strikethrough(component.isStrikethrough())
+                        .underlined(component.isUnderlined())
+                        .font(component.getFont())
+                        .event(new HoverEvent(HoverEvent.Action.SHOW_TEXT,
+                                hoverText
+                        ))
+                        .create()));
+                if (component.toPlainText().length() > 8) {
+                    BaseComponent[] post = new ComponentBuilder(component.toPlainText().substring(8))
+                            .color(component.getColor())
+                            .bold(component.isBold())
+                            .italic(component.isItalic())
+                            .obfuscated(component.isObfuscated())
+                            .strikethrough(component.isStrikethrough())
+                            .underlined(component.isUnderlined())
+                            .font(component.getFont())
+                            .create();
+                    components.addAll(Arrays.asList(post));
+                }
+            } else {
+                components.add(component);
+            }
+        }
+        return components.toArray(new BaseComponent[0]);
+    }
+
     @EventHandler(priority = EventPriority.HIGHEST) // Listening for the event.
     public void onChat(AsyncPlayerChatEvent event) {
         // Called when a player sends a chat message.
         Player player = event.getPlayer(); // Getting the player who sent the message.
+        String originalFormat = event.getFormat();
         String message = event.getMessage();
         PluralKitSystem system = data.getSystem(player.getUniqueId());
         PluralKitMember proxiedMember = data.getProxiedUser(player.getUniqueId(), message);
@@ -84,47 +137,7 @@ public class ProxyListener implements Listener {
             if (config.getBoolean("hover_text", false)) {
                 String resultMessage = String.format(event.getFormat(), player.getDisplayName(), event.getMessage());
                 BaseComponent[] resultComponents = TextComponent.fromLegacyText(resultMessage);
-                ArrayList<BaseComponent> components = new ArrayList<>();
-                for (BaseComponent component : resultComponents) {
-                    if (component.toPlainText().startsWith("%member%")) {
-                        ComponentBuilder hoverTextBuilder = new ComponentBuilder("User: ").color(ChatColor.GREEN)
-                                .append(TextComponent.fromLegacyText(player.getDisplayName())).append("\nSystem: ").color(ChatColor.GREEN);
-                        if (system.getName() != null && system.getName().length() > 0) {
-                            hoverTextBuilder.append(system.getName()).color(ChatColor.AQUA);
-                        } else {
-                            hoverTextBuilder.append(system.getId()).color(ChatColor.GRAY);
-                        }
-                        Text hoverText = new Text(hoverTextBuilder.create());
-                        components.addAll(Arrays.asList(new ComponentBuilder(memberName)
-                                .color(component.getColor())
-                                .bold(component.isBold())
-                                .italic(component.isItalic())
-                                .obfuscated(component.isObfuscated())
-                                .strikethrough(component.isStrikethrough())
-                                .underlined(component.isUnderlined())
-                                .font(component.getFont())
-                                .event(new HoverEvent(HoverEvent.Action.SHOW_TEXT,
-                                        hoverText
-                                ))
-                                .create()));
-                        config.set("test", player.getDisplayName());
-                        if (component.toPlainText().length() > 8) {
-                            BaseComponent[] post = new ComponentBuilder(component.toPlainText().substring(8))
-                                    .color(component.getColor())
-                                    .bold(component.isBold())
-                                    .italic(component.isItalic())
-                                    .obfuscated(component.isObfuscated())
-                                    .strikethrough(component.isStrikethrough())
-                                    .underlined(component.isUnderlined())
-                                    .font(component.getFont())
-                                    .create();
-                            components.addAll(Arrays.asList(post));
-                        }
-                    } else {
-                        components.add(component);
-                    }
-                }
-                BaseComponent[] sendable = components.toArray(new BaseComponent[0]);
+                BaseComponent[] sendable = getOutputComponent(resultComponents, player, system, memberName);
                 for (Player p : event.getRecipients()) {
                     p.spigot().sendMessage(sendable);
                 }
@@ -138,8 +151,28 @@ public class ProxyListener implements Listener {
                         discord.processChatMessage(player, message, "global", false);
                     }
                 }
-                Bukkit.getConsoleSender().spigot().sendMessage(sendable);
-                event.setCancelled(true);
+                if (config.getBoolean("keep_original_message_event", false)) {
+                    /*
+                    Don't cancel the original message event
+                    Just get rid of recipients, if possible,
+                    this means the console will not see the member name
+                     */
+                    try {
+                        event.setFormat(originalFormat);
+                        event.getRecipients().clear();
+                    } catch (UnsupportedOperationException e) {
+                        event.setCancelled(true);
+                    }
+                } else {
+                    if (config.getBoolean("log_username", true)) {
+                        BaseComponent[] loggable = getOutputComponent(resultComponents, player, system, memberName, true);
+                        Bukkit.getConsoleSender().spigot().sendMessage(loggable);
+                    } else {
+                        Bukkit.getConsoleSender().spigot().sendMessage(sendable);
+                    }
+                    event.setCancelled(true);
+                }
+
             }
         }
     }
